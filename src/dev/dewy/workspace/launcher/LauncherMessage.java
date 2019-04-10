9bcd3e0b3c47db46dad370239387508dddbe8726
Dewy <dljcoolguy@gmail.com>
2019-04-10 19:13:35 +0100

    Initial Commit! :D
    + .gitignore
    + Motivation

---
+++ src/dev/dewy/workspace/launcher/LauncherMessage.java
@@ -0,0 +1,14 @@
+package dev.dewy.workspace.launcher;
+
+public class LauncherMessage
+{
+    public static void main(String[] args)
+    {
+        onLaunch();
+    }
+
+    private static void onLaunch()
+    {
+        System.err.println("Keep going! Don't give up! :D");
+    }
+}
